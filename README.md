98095f6c15ea23d9fa02e7393a10dbbad1cdc065
RobertGarabetian <robertgarabetian90@gmail.com>
2024-05-15 22:55:45 -0700

    Connected database to UI

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 ## TODO
-x Make it deploy (vercel)
-x Scaffold basic ui with mock data
-- Actually set up database (vercel postgres)
-- Attach database to ui
-- Add auth (w/ Clerk)
-- Add image upload
-- Error management (w/ Sentry)
-- Routing/image page (parallel route)
-- Delete button (w/ server actions)
-- Analytics (posthog)
-- Ratelimiting (upstash) 
+- [x] Make it deploy (vercel)
+- [x] Scaffold basic ui with mock data
+- [x] Actually set up database (vercel postgres)
+- [x] Attach database to ui
+- [] Add auth (w/ Clerk)
+- [] Add image upload
+- [] Error management (w/ Sentry)
+- [] Routing/image page (parallel route)
+- [] Delete button (w/ server actions)
+- [] Analytics (posthog)
+- [] Ratelimiting (upstash) 
 - 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,18 @@
 import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/dada04cb-e683-45f6-b8e9-3b308864260d-z7dpqd.png",
-  "https://utfs.io/f/dada04cb-e683-45f6-b8e9-3b308864260d-z7dpqd.png",
-  "https://utfs.io/f/dada04cb-e683-45f6-b8e9-3b308864260d-z7dpqd.png",
-  "https://utfs.io/f/dada04cb-e683-45f6-b8e9-3b308864260d-z7dpqd.png",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-60">
-            <img src={image.url} alt="image" />
+        {[...images, ...images, ...images].map((image, index) => (
+          <div key={image.id + "-" + index} className="flex w-48 flex-col">
+            <img src={image.url} alt="Image" />
+            {image.name}
           </div>
         ))}
       </div>
